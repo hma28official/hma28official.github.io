d2cba24758b48cb363aa6c774c051e9aa8de7684
Hilman Maulana Anhar <hma28official@gmail.com>
2022-05-11 12:11:51 +0700

    Software Developer (Intern) Test

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { ReactComponent as LogoGithub } from "../images/logo-github.svg";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a
+        href="https://github.com/hma28official/rest-countries-api-reactjs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <LogoGithub />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
